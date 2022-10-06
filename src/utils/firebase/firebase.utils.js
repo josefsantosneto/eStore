0c0de1261ea629a0268343dbc17af674b752d849
Jose Santos <82674884+josefsantosneto@users.noreply.github.com>
2022-10-06 17:08:41 -0300

    Added firebase and login, created a db and it is saving the user in the db

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,57 @@
+import {initializeApp} from 'firebase/app';
+import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC_9kmik8YthznfRpHdNp74pduH7UsMnFg",
+    authDomain: "crwn-clothing-db-48446.firebaseapp.com",
+    projectId: "crwn-clothing-db-48446",
+    storageBucket: "crwn-clothing-db-48446.appspot.com",
+    messagingSenderId: "212187789281",
+    appId: "1:212187789281:web:d8a810e3c7da2585a684a0"
+  };
+
+
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt:"select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = ()=> signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+//this function below is going to check if there is a document with the same id
+//and is going to create this user document in case of not exist
+
+export const createUserDocumentFromAuth = async (userAuth)=> {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  
+  const userSnapshot = await getDoc(userDocRef);
+  
+  if(!userSnapshot.exists()) {
+    const {displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    }catch(error){
+      console.log('error creating the user', error.message);
+
+    }
+  }
+
+  return userDocRef;
+};
