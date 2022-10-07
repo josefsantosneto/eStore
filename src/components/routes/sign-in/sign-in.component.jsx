ad8d8e81ab6a8373ba6d1012af90b0eaaba8556d
Jose Santos <82674884+josefsantosneto@users.noreply.github.com>
2022-10-07 11:05:08 -0300

    Took out the google redirect button and its functions and came back to the old version of the google popup sign in

--- src/components/routes/sign-in/sign-in.component.jsx
+++ src/components/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,10 @@
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from "../../../utils/firebase/firebase.utils";
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
+import {signInWithGooglePopup, createUserDocumentFromAuth} from "../../../utils/firebase/firebase.utils";
+// import { useEffect } from "react";
+// import { getRedirectResult } from "firebase/auth";
 
 
 const SignIn = ()=>{
-    //this part is quite different than the course as it was showing an error
-    useEffect(()=> {
-        async function fetchData() {
-            const response = await getRedirectResult(auth);
-            
-            if(response) {
-                const userDocRef = await createUserDocumentFromAuth(response.user);
-            }
-        }
-        fetchData();
-    },[]);
+
 
     const logGoogleUser = async ()=>{
         const {user} = await signInWithGooglePopup();
@@ -22,19 +12,14 @@ const SignIn = ()=>{
 
     };
 
-    const logGoogleRedirectUser = async ()=>{
-        const {user} = await signInWithGoogleRedirect();
-        console.log(user);
 
-    };
 
     return(
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign in with Google Popup
             </button>
-            <button onClick={logGoogleRedirectUser}>Sign in with Google Redirect
-            </button>
+        
         </div>
     );
 };
